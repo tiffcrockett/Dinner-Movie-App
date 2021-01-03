0cc3cc5a9343994f05ec17daffc7568e42261c99
Roseanne Nguyen <rtnguye1@gmail.com>
2021-01-02 19:58:41 -0700

    added updates to movie API to append to the page

--- js/script.js
+++ js/script.js
@@ -33,9 +33,14 @@ var foodChoices = {
 }
 
 
-$("#getMovieDinnerInfo").on("click", function(event){
-event.preventDefault();   
+$("#getMovieDinnerInfo").on("click", function (event) {
+    event.preventDefault();
+    getYelp()
+    getMovie()
+    renderInput()
 
+})
+function getMovie() {
 
     var movieTitle = $("#movie-input").val().trim();
     var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + "6cc7def2";
@@ -52,16 +57,19 @@ event.preventDefault();
         var plot = response.Plot;
         var released = response.Released;
         var rating = response.Rated;
+        var genre = response.Genre
 
         var plotInfo = $("<p>").text("Plot: " + plot);
         var releaseInfo = $("<p>").text("Released: " + released);
+        var genreInfo = $("<p>").text("Genre: " + genre);
         var ratingInfo = $("<p>").text("Rating: " + rating);
 
         movieInfoDisplay.append(plotInfo);
         movieInfoDisplay.append(releaseInfo);
+        movieInfoDisplay.append(genreInfo);
         movieInfoDisplay.append(ratingInfo);
 
-        $("#movie-info").append(movieInfoDisplay);
+        $("#movie-view").append(movieInfoDisplay);
 
         var movieImgDisplay = $("<div>");
 
@@ -69,18 +77,16 @@ event.preventDefault();
         var img = $("<img>").attr("src", imgURL);
         movieImgDisplay.append(img);
 
-        $("#movie-image").prepend(movieImgDisplay); 
-
-        // NEED THIS SPECIFIC CLASS TO DISPLAY
-        // <div class="w3-card w3-container w3-margin w3-white"></div>
+        $("#movie-view").prepend(movieImgDisplay);
 
     })
-});
+};
 
-// var baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location=boston"
 
-function testYelp() {
-    var zipCode = $("#zip-code-input").val()
+
+
+function getYelp() {
+    var zipCode = $("#zip-code-input").val().trim();
 
     var baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?cc=US&location=" + zipCode + "&categories=restaurants"
     $.ajax({
@@ -93,13 +99,13 @@ function testYelp() {
         dataType: 'json',
         success: function (data) {
             console.log('success: ' + data);
-
+            console.log(data)
             // Grab the results from the API JSON return
             var totalresults = data.total;
             // If our results are greater than 0, continue
             if (totalresults > 0) {
                 // Display a header on the page with the number of results
-                $('#food-1').append('<h6 style="padding-left:35px;">Here are our dinner suggestions:</h6>');
+                $('#food-1').append('<h5>We discovered ' + totalresults + ' results! Below are our top 3 recommendations:</h5>');
                 // Itirate through the JSON array of 'businesses' which was returned by the API
 
                 $.each(data.businesses, function (i, item) {
@@ -108,38 +114,51 @@ function testYelp() {
                     }
 
                     var id = item.id;
-                    var alias = item.alias;
                     var image = item.image_url;
                     var name = item.name;
+                    var category = item.categories[0].title
+
                     var rating = item.rating;
                     var reviewcount = item.review_count;
+
                     var address = item.location.address1;
                     var city = item.location.city;
                     var state = item.location.state;
                     var zipcode = item.location.zip_code;
-                    // Append our result into our page
-                     // Append our result into our page
-                     return ($('#food-1').append('<div id="' + id + '" style="margin-top:25px;margin-bottom:30px;margin-left:35px;font-size:13px;"><img src="' + image + '" style="width:100px;height:95px;"><br> We found <b>' + name + '</b><br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br> This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>')); 
-                    // NEED THIS SPECIFIC CLASS TO DISPLAY 
-                    //  <div class="w3-card w3-container w3-margin w3-white">
-                                
+                    // Append our result into our page. Easier to call on one call for Yelp API.
+                    return ($('#food-1').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;justify-content: center;"><img src="' + image + '" style="width:200px;height:150px;justify-content: center;"><br>We found <b>' + name + ' under ' + category + ' Category</b> <br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>')
+                    
+                    
+                    );
+                
+                
                 });
+
             }
         }
     });
+
 }
+
+
+
+// draft code. Might have to call both queryurl again 
+// function compareTest() {
+//     var genreType = response.genre
+
+// if (genreType === action) {
+//     append food category
+// }
+
+// }
+
+
 
 function renderInput() {
-$("#food-1").empty()
-$("getMovieDinnerInfo").empty()
+    $("#food-1").empty()
+    $("#movie-view").empty()
+    $("getMovieDinnerInfo").empty()
 }
-
-$("#getMovieDinnerInfo").on("click", function (event) {
-    event.preventDefault();
-    testYelp()
-    renderInput()
-
-})
 
 
 
