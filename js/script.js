9aaa3ed00e7091597408dfd1d55e90921771d3d0
Tiffany Crockett <crockett.tiff@gmail.com>
2021-01-03 17:19:52 -0700

    update html - add page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-
-
 var slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = $(".mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 3 seconds
+    var i;
+    var x = $(".mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds
 }
 
 
@@ -33,18 +31,23 @@ var foodChoices = {
 }
 
 
-$("#getMovieDinnerInfo").on("click", function(event){
-event.preventDefault();   
+$("#getMovieDinnerInfo").on("click", function (event) {
+    event.preventDefault();
+    getYelp()
+    getMovie()
+    renderInput()
 
-
+})
+function getMovie(genre) {
+    var genre = response.genre
     var movieTitle = $("#movie-input").val().trim();
-    var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + "6cc7def2";
+    var queryURL = "https://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + "6cc7def2";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
+        console.log(queryURL)
         console.log(response);
 
         var movieInfoDisplay = $("<div>");
@@ -52,16 +55,19 @@ event.preventDefault();
         var plot = response.Plot;
         var released = response.Released;
         var rating = response.Rated;
+        var genre = response.Genre
 
         var plotInfo = $("<p>").text("Plot: " + plot);
         var releaseInfo = $("<p>").text("Released: " + released);
+        var genreInfo = $("<p>").text("Genre: " + genre);
         var ratingInfo = $("<p>").text("Rating: " + rating);
 
         movieInfoDisplay.append(plotInfo);
         movieInfoDisplay.append(releaseInfo);
+        movieInfoDisplay.append(genreInfo);
         movieInfoDisplay.append(ratingInfo);
 
-        $("#movie-info").append(movieInfoDisplay);
+        $("#movie-view").append(movieInfoDisplay);
 
         var movieImgDisplay = $("<div>");
 
@@ -69,18 +75,14 @@ event.preventDefault();
         var img = $("<img>").attr("src", imgURL);
         movieImgDisplay.append(img);
 
-        $("#movie-image").prepend(movieImgDisplay); 
-
-        // NEED THIS SPECIFIC CLASS TO DISPLAY
-        // <div class="w3-card w3-container w3-margin w3-white"></div>
+        $("#movie-view").prepend(movieImgDisplay);
 
     })
-});
+};
 
-// var baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location=boston"
 
-function testYelp() {
-    var zipCode = $("#zip-code-input").val()
+function getYelp() {
+    var zipCode = $("#zip-code-input").val().trim();
 
     var baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?cc=US&location=" + zipCode + "&categories=restaurants"
     $.ajax({
@@ -92,8 +94,7 @@ function testYelp() {
         method: 'GET',
         dataType: 'json',
         success: function (data) {
-            console.log('success: ' + data);
-
+            console.log(data)
             // Grab the results from the API JSON return
             var totalresults = data.total;
             // If our results are greater than 0, continue
@@ -107,39 +108,41 @@ function testYelp() {
                         return false;
                     }
 
-                    var id = item.id;
-                    var alias = item.alias;
                     var image = item.image_url;
                     var name = item.name;
+                    var category = item.categories[0].title
+
                     var rating = item.rating;
                     var reviewcount = item.review_count;
+
                     var address = item.location.address1;
                     var city = item.location.city;
                     var state = item.location.state;
                     var zipcode = item.location.zip_code;
-                    // Append our result into our page
-                     // Append our result into our page
-                     return ($('#food-1').append('<div id="' + id + '" style="margin-top:25px;margin-bottom:30px;margin-left:35px;font-size:13px;"><img src="' + image + '" style="width:100px;height:95px;"><br> We found <b>' + name + '</b><br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br> This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>')); 
-                    // NEED THIS SPECIFIC CLASS TO DISPLAY 
-                    //  <div class="w3-card w3-container w3-margin w3-white">
-                                
+                    // Append our result into our page. Easier to call on one call for Yelp API. Need to random generate
+                    return ($('#food-1').append('<div style="margin-top:25px;margin-bottom:30px;margin-left:35px;font-size:13px;"><img src="' + image + '" style="width:100px;height:95px;justify-content:center;"><br><b>' + name + ' under ' + category + ' Category</b> <br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>')
+
+
+                    );
+
+
                 });
+
             }
         }
     });
+
 }
+
+// note: testing pulling api together
+    // var genre = response.Genre
+    // var category = item.categories[0].title
+
+
 
 function renderInput() {
-$("#food-1").empty()
-$("getMovieDinnerInfo").empty()
+    $("#food-1").empty()
+    $("#movie-view").empty()
+    $("getMovieDinnerInfo").empty()
 }
-
-$("#getMovieDinnerInfo").on("click", function (event) {
-    event.preventDefault();
-    testYelp()
-    renderInput()
-
-})
-
-
 
